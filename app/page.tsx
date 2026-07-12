17bfdc253ebdecfaf1528221566daa0bd836f011
x-Alex-Ab-x <alexandra_abra@hotmail.com>
2026-07-12 08:35:05 -0400

    Create custom homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,35 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="min-h-screen bg-zinc-950 text-white">
+      <section className="mx-auto flex min-h-screen max-w-5xl flex-col justify-center px-6">
+        <p className="mb-4 text-sm font-medium uppercase tracking-wide text-violet-400">
+          Job Tracker App
+        </p>
+
+        <h1 className="mb-6 text-1xl font-bold tracking-tight md:text-5xl">
+          Suivre mes candidatures et gérer ma recherche d'emploi en informatique
+        </h1>
+
+        <p className="mb-8 max-w-2xl text-lg text-slate-300">
+          C'est une application pour m'aider à:
+          <ul className="list-disc space-y-1 pl-6">
+            <li>organiser mes candidatures</li>
+            <li>suivre les statuts</li>
+            <li>donner une note aux entreprises</li>
+            <li>mieux gérer mes recherches d'emploi</li>
+          </ul>
+        </p>
+
+        <div className="mt-8 flex flex-wrap gap-4">
+          <button className="rounded-lg bg-violet-600 px-5 py-3 font-medium text-white hover:bg-violet-700">
+            Ajouter une candidature
+          </button>
+
+          <button className="rounded-lg border border-violet-500 px-5 py-3 font-medium text-violet-100 hover:bg-violet-950">
+            Voir mes candidatures
+          </button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </section>
+    </main>
   );
 }
