4ee3c8d5def405d00f3dd2232ecb2c9bee34bb28
x-Alex-Ab-x <alexandra_abra@hotmail.com>
2026-07-14 18:07:40 -0400

    Link homepage to applications page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-zinc-950 text-white">
@@ -6,28 +8,33 @@ export default function Home() {
           Job Tracker App
         </p>
 
-        <h1 className="mb-6 text-1xl font-bold tracking-tight md:text-5xl">
-          Suivre mes candidatures et gérer ma recherche d'emploi en informatique
+        <h1 className="mb-6 text-4xl font-bold tracking-tight md:text-5xl">
+          Suivre mes candidatures et gérer ma recherche d&apos;emploi en
+          informatique
         </h1>
 
-        <p className="mb-8 max-w-2xl text-lg text-slate-300">
-          C'est une application pour m'aider à:
-          <ul className="list-disc space-y-1 pl-6">
-            <li>organiser mes candidatures</li>
-            <li>suivre les statuts</li>
-            <li>donner une note aux entreprises</li>
-            <li>mieux gérer mes recherches d'emploi</li>
+        <div className="mb-8 max-w-2xl text-lg text-slate-300">
+          <p>C&apos;est une application pour m&apos;aider à :</p>
+
+          <ul className="mt-4 list-disc space-y-1 pl-6">
+            <li>organiser mes candidatures;</li>
+            <li>suivre les statuts;</li>
+            <li>donner une note aux entreprises;</li>
+            <li>mieux gérer mes recherches d&apos;emploi.</li>
           </ul>
-        </p>
+        </div>
 
         <div className="mt-8 flex flex-wrap gap-4">
           <button className="rounded-lg bg-violet-600 px-5 py-3 font-medium text-white hover:bg-violet-700">
             Ajouter une candidature
           </button>
 
-          <button className="rounded-lg border border-violet-500 px-5 py-3 font-medium text-violet-100 hover:bg-violet-950">
+          <Link
+            href="/applications"
+            className="rounded-lg border border-violet-500 px-5 py-3 font-medium text-violet-100 hover:bg-violet-950"
+          >
             Voir mes candidatures
-          </button>
+          </Link>
         </div>
       </section>
     </main>
